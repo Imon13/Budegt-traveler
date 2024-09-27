b1737d3b8e40f029bb46e4f53c28cacdc2617267
imon <imonsaha153@gmail.com>
2024-09-27 09:15:26 +0600

    display name and price in cart section done

--- script.js
+++ script.js
@@ -9,6 +9,19 @@ for (const btn of allBtn) {
         count = count+1;
         console.log(count)
        setInnerText('cart-count',count)
+       const placeName = e.target.parentNode.childNodes[1].innerText
+       console.log(placeName)
+       const placePrice =e.target.parentNode.childNodes[3].childNodes[1].innerText
+       console.log(placePrice)
+       const selectedContainer = document.getElementById('selected-place-container')
+       const li = document.createElement('li')
+       const p = document.createElement('p')
+       p.innerText = placeName
+       const p2 = document.createElement('p')
+       p2.innerText =`${placePrice}$`
+       li.appendChild(p)
+       li.appendChild(p2)
+       selectedContainer.appendChild(li)
     })
     
 }
